b579a1a044ddefa7ba9661d59692d06423a4d959
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-21 22:31:58 +0500

    correct patterns

--- spicy/dom/html/enum_.py
+++ spicy/dom/html/enum_.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 
 class HTMLPatterns(Enum):
-    ATTRS_PATTERN: re.Pattern = re.compile(r"(?P<name>[\-_\w]*)\s*?=\s*?(?P<comma>[\"\'])(?P<value>.*?)(?P=comma)")
+    ATTRS_PATTERN: re.Pattern = re.compile(r"\s*(?P<name>.*?)\s*=\s*(?P<comma>[\"\'])(?P<value>.*?)(?P=comma)")
     TAG_PATTERN: re.Pattern = re.compile(r'<(?P<tag>[\w\-_]+)(?P<attrs>[^>]*)>(?P<inner_tags>.*)</\1>', re.DOTALL)
     TAG_UNCLOSED_PATTERN: re.Pattern = re.compile(r"<(\w*)\s*(.*?)>")
     INNER_TAG_PATTERN: re.Pattern = re.compile(r"(<([/\-_a-zA-Z1-6!-]+)[^>]*>)")
